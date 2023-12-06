2a20b9197713cfd2b1d4bab50382cdc86e78471d
Tzvi (Steve) Neiman <tneiman19@gmail.com>
2023-12-06 02:00:30 -0500

    Style: Implemented Shadcn UI

--- app/entity/_components/EntityForm.tsx
+++ app/entity/_components/EntityForm.tsx
@@ -1,28 +1,29 @@
 "use client";
 import { postEntitySchema } from "@/app/api/entity/validateEntity";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, Checkbox, Input, Textarea } from "@nextui-org/react";
 import { Entity } from "@prisma/client";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import toast, { Toaster } from "react-hot-toast";
-import { z } from "zod";
+import * as z from "zod";
 
 type EntityFormData = z.infer<typeof postEntitySchema>;
 
 const EntityForm = ({ entity }: { entity?: Entity }) => {
-  const router = useRouter();
   const {
     register,
-    control,
     handleSubmit,
-    setValue,
     formState: { errors },
   } = useForm<EntityFormData>({
     resolver: zodResolver(postEntitySchema),
   });
+
+  const router = useRouter();
 
   const [error, setError] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -32,100 +33,106 @@ const EntityForm = ({ entity }: { entity?: Entity }) => {
       setIsSubmitting(true);
       if (entity) await axios.patch(`/api/entity/${entity.id}`, data);
       else await axios.post("/api/entity", data);
-      toast.success("Entity saved successfully")
-      // console.log(data)
-      router.push("/entity"); //redirect to entity page
+      //console.log(data);
+      router.push("/entity");
       router.refresh();
     } catch (error: any) {
       setIsSubmitting(false);
       setError(error.response.data.errorMessage);
-      toast.error("Error saving entity");
       //console.log(data);
-      console.log(error);
+      //console.log(error);
     }
   });
 
   return (
     <>
-      <Toaster />
-      <form className="p-4 mx-auto md:max-w-xl sm:max-w-sm border border-gray-300 rounded-lg">
+      <form
+        onSubmit={onSubmit}
+        className="p-4 mx-auto md:max-w-xl sm:max-w-sm border border-gray-300 rounded-lg shadow-sm"
+      >
         <h1 className="text-center text-2xl font-semibold text-gray-700 mb-4">
           Entity Form
         </h1>
         {error && <p className="text-center text-red-500 ">{error}</p>}
-
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 ">
           <div>
+            <Label>Entity Name</Label>
             <Input
-              isRequired
-              label="Entity Name"
               defaultValue={entity?.entityName}
-              className=""
               {...register("entityName")}
             />
-            {errors.entityName && (
+            {errors && (
               <p className="text-center text-red-500 ">
-                {errors.entityName.message}
+                {errors.entityName?.message}
               </p>
             )}
           </div>
-          <Input
-            label="Contact Name"
-            defaultValue={entity?.contactName || undefined}
-            className=""
-            {...register("contactName")}
-          />
-          <Input
-            label="Contact Email"
-            defaultValue={entity?.contactEmail || undefined}
-            className=""
-            {...register("contactEmail")}
-          />
-          <Input
-            label="Contact Phone"
-            defaultValue={entity?.contactPhone || undefined}
-            className=""
-            {...register("contactPhone")}
-          />
-          <Input
-            label="Address"
-            defaultValue={entity?.address || undefined}
-            className=""
-            {...register("address")}
-          />
 
-          <Input
-            label="City"
-            defaultValue={entity?.city || undefined}
-            className=""
-            {...register("city")}
-          />
-          <Input
-            label="State"
-            defaultValue={entity?.state || undefined}
-            className=""
-            {...register("state")}
-          />
-          <Input
-            label="Zip Code"
-            defaultValue={entity?.zip || undefined}
-            className=""
-            {...register("zip")}
+          <div>
+            <Label>Contact Name</Label>
+            <Input
+              defaultValue={entity?.contactName || undefined}
+              {...register("contactName")}
+            />
+            {errors.contactName && <p>{errors.contactName.message}</p>}
+          </div>
+
+          <div>
+            <Label>Contact Email</Label>
+            <Input
+              defaultValue={entity?.contactEmail || ""}
+              {...register("contactEmail")}
+            />
+            {errors.contactEmail && <p>{errors.contactEmail.message}</p>}
+          </div>
+
+          <div>
+            <Label>Contact Phone</Label>
+            <Input
+              defaultValue={entity?.contactPhone || ""}
+              {...register("contactPhone")}
+            />
+            {errors.contactPhone && <p>{errors.contactPhone.message}</p>}
+          </div>
+
+          <div>
+            <Label>Address</Label>
+            <Input
+              defaultValue={entity?.address || ""}
+              {...register("address")}
+            />
+            {errors.address && <p>{errors.address.message}</p>}
+          </div>
+
+          <div>
+            <Label>City</Label>
+            <Input defaultValue={entity?.city || ""} {...register("city")} />
+            {errors.city && <p>{errors.city.message}</p>}
+          </div>
+
+          <div>
+            <Label>State</Label>
+            <Input defaultValue={entity?.state || ""} {...register("state")} />
+            {errors.state && <p>{errors.state.message}</p>}
+          </div>
+
+          <div>
+            <Label>Zip Code</Label>
+            <Input defaultValue={entity?.zip || ""} {...register("zip")} />
+            {errors.zip && <p>{errors.zip.message}</p>}
+          </div>
+        </div>
+
+        <div className="gap-4 p-4">
+          <Label>Entity Notes </Label>
+
+          <Textarea
+            defaultValue={entity?.entityNote || ""}
+            {...register("entityNote")}
           />
         </div>
-        <Textarea
-          label="Entity Notes"
-          defaultValue={entity?.entityNote || undefined}
-          className="gap-4 p-4"
-          {...register("entityNote")}
-        />
         <div className="grid lg:grid-cols-3 gap-4 p-4">
-          <Button
-            onClick={onSubmit}
-            color="primary"
-            isLoading={isSubmitting}
-            className="max-w-xs col-start-3"
-          >
+          <Button className="max-w-xs col-start-3" disabled={isSubmitting}>
             {entity ? "Update Entity" : "Register Entity"}{" "}
           </Button>
         </div>
