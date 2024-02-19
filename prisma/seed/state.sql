91a0dd9188bdbabcd09967bf4be7c4d9b1d6a444
Tzvi (Steve) Neiman <tneiman19@gmail.com>
2024-02-18 23:39:04 -0500

    feat(scripts): add script to seed states into database

---
+++ prisma/seed/state.sql
@@ -0,0 +1,82 @@
+drop table if exists "temp_state";
+
+CREATE TEMP TABLE "temp_state" (
+    abbreviation VARCHAR(2),
+    name VARCHAR(100)
+);
+
+insert
+	into
+	"temp_state" (abbreviation,
+	name)
+values
+('AL', 'Alabama'),
+('AK', 'Alaska'),
+('AZ', 'Arizona'),
+('AR', 'Arkansas'),
+('CA', 'California'),
+('CO', 'Colorado'),
+('CT', 'Connecticut'),
+('DE', 'Delaware'),
+('FL', 'Florida'),
+('GA', 'Georgia'),
+('HI', 'Hawaii'),
+('ID', 'Idaho'),
+('IL', 'Illinois'),
+('IN', 'Indiana'),
+('IA', 'Iowa'),
+('KS', 'Kansas'),
+('KY', 'Kentucky'),
+('LA', 'Louisiana'),
+('ME', 'Maine'),
+('MD', 'Maryland'),
+('MA', 'Massachusetts'),
+('MI', 'Michigan'),
+('MN', 'Minnesota'),
+('MS', 'Mississippi'),
+('MO', 'Missouri'),
+('MT', 'Montana'),
+('NE', 'Nebraska'),
+('NV', 'Nevada'),
+('NH', 'New Hampshire'),
+('NJ', 'New Jersey'),
+('NM', 'New Mexico'),
+('NY', 'New York'),
+('NC', 'North Carolina'),
+('ND', 'North Dakota'),
+('OH', 'Ohio'),
+('OK', 'Oklahoma'),
+('OR', 'Oregon'),
+('PA', 'Pennsylvania'),
+('RI', 'Rhode Island'),
+('SC', 'South Carolina'),
+('SD', 'South Dakota'),
+('TN', 'Tennessee'),
+('TX', 'Texas'),
+('UT', 'Utah'),
+('VT', 'Vermont'),
+('VA', 'Virginia'),
+('WA', 'Washington'),
+('WV', 'West Virginia'),
+('WI', 'Wisconsin'),
+('WY', 'Wyoming');
+
+insert
+	into
+	"State" (abbreviation,
+	name)
+select
+	"abbreviation",
+	"name"
+from
+	"temp_state" t
+where
+	not exists (
+	select
+		*
+	from
+		"State" s
+	where
+		s."abbreviation" = t."abbreviation");
+
+drop table if exists "temp_state"
